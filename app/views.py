fe2aa37824d08c3614a93dc310374c7f4904d713
Kelandra Pringle <kelandrapringle@gmail.com>
2026-03-02 16:40:41 -0500

    Edited files

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from app import app
+from app import app, mail
 from flask import render_template, request, redirect, url_for, flash
-
+from app.forms import ContactForm
+from flask_mail import Message
 
 ###
 # Routing for your application.
@@ -11,17 +12,14 @@ def home():
     """Render website's home page."""
     return render_template('home.html')
 
-
 @app.route('/about/')
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
-
 ###
 # The functions below should be applicable to all Flask apps.
 ###
-
 
 # Flash errors from the form if validation fails
 def flash_errors(form):
@@ -32,25 +30,49 @@ def flash_errors(form):
                 error
             ), 'danger')
 
-
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
     """Send your static text file."""
     file_dot_text = file_name + '.txt'
     return app.send_static_file(file_dot_text)
 
-
 @app.after_request
 def add_header(response):
     """
     Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also tell the browser not to cache the rendered page. If we wanted
-    to we could change max-age to 600 seconds which would be 10 minutes.
+    and also tell the browser not to cache the rendered page.
     """
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
     response.headers['Cache-Control'] = 'public, max-age=0'
     return response
 
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+
+    if form.validate_on_submit():
+        name    = form.name.data
+        email   = form.email.data
+        subject = form.subject.data
+        message = form.message.data
+
+        msg = Message(
+            subject=subject,
+            sender=(name, email),
+            recipients=[app.config['MAIL_USERNAME']]
+        )
+        msg.body = message
+
+        try:
+            mail.send(msg)
+            flash("Your message was successfully sent!", "success")
+            return redirect(url_for('home'))
+        except Exception as e:
+            flash(f"Error sending email: {str(e)}", "danger")
+    else:
+        flash_errors(form)
+
+    return render_template('contact.html', form=form)
 
 @app.errorhandler(404)
 def page_not_found(error):
